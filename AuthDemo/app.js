38f852ffef18e5260f787180a64cecdb4c087ed5
Joe Scott <jbs198585@gmail.com>
2019-09-07 22:04:00 -0400

    created user model and configured Passport

--- AuthDemo/app.js
+++ AuthDemo/app.js
@@ -1,9 +1,27 @@
-var express = require("express");
-var mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost/auth_demo_app", {useNewUrlParser: true});
+var express = require("express"),
+    mongoose = require("mongoose"),
+    passport = require("passport"),
+    bodyParser = require("body-parser"),
+    User = require("./models/user"),
+    LocalStrategy = require("passport-local"),
+    passportLocalMongoose = require("passport-local-mongoose")
 
+mongoose.connect("mongodb://localhost/auth_demo_app", {useNewUrlParser: true});
 var app = express();
 app.set('view engine', 'ejs');
+
+app.use(require("express-session")({
+  secret: "He thrusts his fists against the posts and still insists he sees the ghosts.",
+  resave: false,
+  saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 
 app.get("/", function(req, res){
   res.render("home");
